533db688973f55459bc5b92c90b9f88f5a43eea5
zoyriver19 <zoyriver19@gmail.com>
2025-04-08 17:48:52 +0800

    更新网页代码，添加了新功能/优化了样式等（此处写具体更新内容）

--- qr.py
+++ qr.py
@@ -1,4 +1,5 @@
 import qrcode
+from PIL import Image, ImageDraw
 
 # 你的网页链接
 url = "https://zoyriver19.github.io/zoyriver19/"
@@ -18,5 +19,35 @@ qr.make(fit=True)
 # 创建图像
 img = qr.make_image(fill='black', back_color='white')
 
+# 打开图标图像
+icon = Image.open("static\images\douyin.jpg")  # 确保你有一个名为 douyin.jpg 的图标文件
+
+# 确保图标是RGBA模式（包含透明度通道）
+if icon.mode != 'RGBA':
+    icon = icon.convert('RGBA')
+
+# 计算图标的位置
+img_w, img_h = img.size
+icon_w, icon_h = icon.size
+
+# 调整图标大小，确保不会覆盖过多的二维码数据
+icon_size = int(img_w * 0.2)
+icon = icon.resize((icon_size, icon_size), Image.LANCZOS)
+icon_w, icon_h = icon.size
+
+# 计算图标的位置，确保不完全覆盖二维码中心
+icon_x = (img_w - icon_w) // 2
+icon_y = (img_h - icon_h) // 2
+
+# 创建一个与二维码相同大小的RGBA图像
+img_with_icon = Image.new('RGBA', (img_w, img_h), (255, 255, 255, 0))
+img_with_icon.paste(img, (0, 0))
+
+# 将图标粘贴到二维码中间，但不完全覆盖
+img_with_icon.paste(icon, (icon_x, icon_y), icon)
+
+# 将带有图标的二维码转换回RGB模式
+img_with_icon = img_with_icon.convert('RGB')
+
 # 保存图像
-img.save("my_website_qrcode.png")
+img_with_icon.save("my_website_qrcode.png")
